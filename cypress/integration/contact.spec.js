048d3e2741c178b8b77b727ddee8d1c4b385b0aa
Luiz Lohn <luiz.lohn@gmail.com>
2023-02-22 15:26:10 -0300

    Create a project without pages

--- cypress/integration/contact.spec.js
+++ cypress/integration/contact.spec.js
@@ -1,47 +1,84 @@
-import ContactPage from '../fixtures/contact_page';
 require('../support/e2e.js');
 
 context('Fields Validation', () => {
-  it('"Name" is mandatory and a message should be displayed in case this field in left empt ', () => {
+  beforeEach(function () {
     cy.visitContact();
-    new ContactPage().getNameFieldValidation();
+    cy.contains('button', 'Send').click();
+  });
+  
+  it('"Name" is mandatory and a message should be displayed in case this field in left empt ', () => {
+    cy.get('[data-testid=form]')
+      .get(':nth-child(1) > :nth-child(1) > .input > .ui').should('have.text', 'Name is mandatory');
   });
   it('"Email" is mandatory and a message should be displayed in case this field in left empt ', () => {
     cy.visitContact();
-
-    new ContactPage().getEmailValidation();
+    cy.get('[data-testid=form]')
+      .get('.eight > .input > .ui').should('have.text', 'Email is mandatory');
   });
   it('"Comment" is mandatory and a message should be displayed in case this field in left empt ', () => {
     cy.visitContact();
-
-    new ContactPage().getCommentsValidation();
+    cy.get('[data-testid=form]')
+      .get(':nth-child(4) > .input > .ui').should('have.text', 'Comment is mandatory');
   });
   it('"Phone" is mandatory and a message should be displayed in case this field in left empt ', () => {
     cy.visitContact();
-
-    new ContactPage().getPhoneValidation();
+    cy.get('[data-testid=form]')
+      .get('.PhoneInput').should('have.text', 'Phone is mandatory');
   });
   it('Should be possible set the data Travel', () => {
     cy.visitContact();
 
-    new ContactPage().setDataTravel();
+    cy.get('.DateRangePickerInput_calendarIcon').click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get("[aria-label='Friday, April 14, 2023']").click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get("[aria-label='Wednesday, June 14, 2023']").click();
   });
   it('Should be possible add text', () => {
     cy.visitContact();
-
-    new ContactPage().setComments();
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4
+      }
+    });
+    cy.get('.input > textarea').type(lorem.generateSentences(5));
   });
 });
 
 context('Send a contact forms', () => {
   it('"Send a forms with all fields fill ', () => {
     cy.visitContact();
-    new ContactPage().setName('Name to use');
-    new ContactPage().setEmail('Email@test.com.br');
-    new ContactPage().setPhone('4842166155');
-    new ContactPage().setGuests('5');
-    new ContactPage().setComments();
-    new ContactPage().setDataTravel();
+    cy.get('[data-testid="form"] > :nth-child(1) > :nth-child(1) > .ui > input').type(name);
+    cy.get('.eight > .ui > input').type(email);
+    cy.get('.PhoneInput').type(phone);
+    cy.get('.four > .ui > input').type(guests);
+    
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4
+      }
+    });
+    cy.get('.input > textarea').type(lorem.generateSentences(5));
+
+    cy.get('.DateRangePickerInput_calendarIcon').click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get("[aria-label='Friday, April 14, 2023']").click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get('.DayPickerNavigation_rightButton__horizontalDefault > .DayPickerNavigation_svg__horizontal').click();
+    cy.get("[aria-label='Wednesday, June 14, 2023']").click();
+
+
     cy.contains('button', 'Send').click();
     cy.get('[data-testid="form"] > .success', { timeout: 10000 }).and('be.visible').should('contain', 'Your request has been sent successfully.');
 });
--- cypress/integration/pricing.spec.js
+++ cypress/integration/pricing.spec.js
@@ -4,17 +4,32 @@ require('../support/e2e.js');
 context('Fields Validation', () => {
   it('Should Display the correct value of price', () => {
     cy.visitPricing();
-
-    new PricingPage().validateYearlyPlanValue();
+    cy.get('[data-price-period="2"]').contains('Yearly');
+    cy.get('#scroll-prop-plan').clear().type('50');
+    cy.contains('.price-card-starter > .price-item', '$64');
+    cy.contains(':nth-child(2) > .price-item', '$375');
+    cy.contains(':nth-child(3) > .price-item', '$525');
   });
   it('Currency properly changes the currency of the pricing options', () => {
     cy.visitPricing();
-
-    new PricingPage().verifyCurrenceChange();
+    cy.get('.price-card-starter > .price-item > :nth-child(1) > .plan-price').invoke('text').then(($value_1) => {
+      cy.get('.price-currency-select').select('gbp')
+      cy.get('.price-card-starter > .price-item > :nth-child(1) > .plan-price').invoke('text').then(($value_2) => { 
+        expect($value_1).to.not.equal($value_2)
+      })
+    })
   });
   it('Months changes pricing options', () => {
     cy.visitPricing();
 
-    new PricingPage().verifyChangeMonths();
+    cy.get('.price-card-starter > .price-item > :nth-child(1) > .plan-price').then(($value) => {
+      const getText = $value.text();
+      if (getText === '$12') {
+        cy.get('#scroll-prop-plan').clear().type('70');
+        const change = cy.get('.price-card-starter > .price-item > :nth-child(1) > .plan-price');
+        change.contains('$74');
+      }
+    }
+    );
   });
 });
